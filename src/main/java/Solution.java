691a3c47b41b05430aeaa67bee95debbd77dd0de
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-01-01 14:49:38 -0300

    Refactor to reduce time and space complexity

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,25 +1,23 @@
 class Solution {
     public int maxScore(String s) {
-        int n = s.length();
+        int oneCount = 0;
 
-        int[] zeroes = new int[n -1];
-        if (s.charAt(0) == '0') zeroes[0] = 1;
-        for (int i = 1; i < zeroes.length; i++) {
-            zeroes[i] = zeroes[i - 1] + (s.charAt(i) == '0' ? 1 : 0);
+        for (char c : s.toCharArray()) {
+            if (c == '1') oneCount++;
         }
 
-        int[] ones = new int[n -1];
-        if (s.charAt(n - 1) == '1') ones[n - 2] = 1;
-        for (int i = n - 3; i >= 0; i--) {
-            ones[i] = ones[i + 1] + (s.charAt(i + 1) == '1' ? 1 : 0);
+        int leftZeroes = 0;
+        int rightOnes = oneCount;
+        int maxScore = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0') {
+                leftZeroes++;
+            } else {
+                rightOnes--;
+            }
+            maxScore = Math.max(maxScore, leftZeroes + rightOnes);
         }
 
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n - 1; i++) {
-            max = Math.max(max, zeroes[i] + ones[i]);
-        }
-
-        return max;
+        return maxScore;
     }
-
 }
